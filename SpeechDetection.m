c0ce03a0287e39082f744c764358a3c8036ec3d4
Louis Keith <louiskeith99@gmail.com>
2022-04-08 15:37:38 -0400

    got text to speech working in a test environment

--- SpeechDetection.m
+++ SpeechDetection.m
@@ -13,17 +13,34 @@ load("commandNet.mat")
 % Listen to the command
 sound(x,fs)
 % Extract auditory spectrum from the sound
-auditorySpect = helperExtractAuditoryFeatures(x,fs);
-% Classify the command based on the spectrogram
-command = classify(trainedNet,auditorySpect);
+% auditorySpect = helperExtractAuditoryFeatures(x,fs);
+% % Classify the command based on the spectrogram
+% command = classify(trainedNet,auditorySpect);
 
 %% Second Example
 x = audioread("play_command.flac");
 sound(x,fs)
-auditorySpect = helperExtractAuditoryFeatures(x,fs);
-command = classify(trainedNet,auditorySpect);
+% auditorySpect = helperExtractAuditoryFeatures(x,fs);
+% command = classify(trainedNet,auditorySpect);
 
 %% Third Example
 x = pinknoise(16e3);
 auditorySpect = helperExtractAuditoryFeatures(x,fs);
 command = classify(trainedNet,auditorySpect);
+
+%% Experimentation
+[y, Fs] = audioread('Up.mp3');
+player = audioplayer(y, Fs);
+play(player)
+%%
+[y, Fs] = audioread('Down.mp3');
+player = audioplayer(y, Fs);
+play(player)
+%%
+[y, Fs] = audioread('Left.mp3');
+player = audioplayer(y, Fs);
+play(player)
+%%
+[y, Fs] = audioread('Right.mp3');
+player = audioplayer(y, Fs);
+play(player)
